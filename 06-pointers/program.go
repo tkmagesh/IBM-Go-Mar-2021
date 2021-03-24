645fc533447d5d0fc44f0bf907c52d7ab37cc57e
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-24 16:37:57 +0530

    pointers - 2

--- 06-pointers/program.go
+++ 06-pointers/program.go
@@ -12,6 +12,15 @@ func main() {
 	fmt.Printf("Before swapping n1=%d n2=%d\n", n1, n2)
 	swap(&n1, &n2)
 	fmt.Printf("After swapping n1=%d n2=%d\n", n1, n2)
+
+	nos := []int{10, 20, 30}
+	fmt.Printf("Initial list %v\n", nos)
+	insert(&nos, 100)
+	fmt.Printf("After appending 100, list = %v\n", nos)
+}
+
+func insert(listPtr *[]int, no int) {
+	*listPtr = append(*listPtr, no)
 }
 
 func swap(n1, n2 *int) {
@@ -19,4 +28,8 @@ func swap(n1, n2 *int) {
 	*n1 = *n2
 	*n2 = temp
 	fmt.Printf("[@swap] after swapping n1=%d n2=%d\n", *n1, *n2)
+}
+
+func swap2(n1, n2 int) (int, int) {
+	return n2, n1
 }
