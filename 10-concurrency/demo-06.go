058417ec8eab13d409a2d33d22a1e5d8ff284621
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-25 15:25:22 +0530

    assignment - solution

--- 10-concurrency/demo-06.go
+++ 10-concurrency/demo-06.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func print1(s string, c1 chan string, c2 chan string) {
+func print(s string, c1 chan string, c2 chan string) {
 	for i := 0; i < 5; i++ {
 		<-c1
 		fmt.Println(s)
@@ -14,22 +14,12 @@ func print1(s string, c1 chan string, c2 chan string) {
 	}
 }
 
-func print2(s string, c1 chan string, c2 chan string) {
-	for i := 0; i < 5; i++ {
-		<-c2
-		fmt.Println(s)
-		time.Sleep(1000 * time.Millisecond)
-		c1 <- "done"
-	}
-}
-
 func main() {
 	c1 := make(chan string)
 	c2 := make(chan string)
-	go print1("World", c1, c2)
-	go print2("Hello", c1, c2)
+	go print("World", c1, c2)
+	go print("Hello", c2, c1)
 	c2 <- "start"
-	//go print("Magesh")
 	var input string
 	fmt.Scanln(&input)
 	fmt.Println("end of main")
