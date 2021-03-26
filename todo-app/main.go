ee86493669e12a87c3711996e309c6f9e7481616
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-26 17:27:16 +0530

    api server in go

---
+++ todo-app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	config "todo-app/config"
+	models "todo-app/models"
+	routes "todo-app/routes"
+
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main() {
+	config.DB, err = gorm.Open("mysql", config.DbURL(config.BuildConfig()))
+
+	if err != nil {
+		log.Fatalf("Error connecting to database : %s", err.Error())
+	}
+	defer config.DB.Close()
+	config.DB.AutoMigrate(&models.Todo{})
+	r := routes.SetupRouter()
+
+	r.Run()
+}
